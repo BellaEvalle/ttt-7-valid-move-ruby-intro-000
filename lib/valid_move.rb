98c0fa88acbf2489f67cb8ea3b10107ab9afa687
Bella Evalle <bellaevalle@yahoo.com>
2020-03-08 03:26:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,10 +7,16 @@ def valid_move?(board, index)
     end
   end
 
-  def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    return false
-  else
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
     return true
+  else
+    return false
   end
 end
